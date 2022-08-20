2d86df9792c79eabc5e1d17aa5c84712de817677
bonveeee <bonochieng@gmail.com>
2022-08-20 06:58:17 +0300

    to be completed

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,16 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    # array = [1,2,3,4]
+    # element = 5
+    # arrays = array.push(element)
+        
 end
 
 def add_element_to_start_of_array(array, element)
